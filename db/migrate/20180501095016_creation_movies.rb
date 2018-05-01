690fb4ef01b3e9d969f4667cd43fa59754249fb8
Manu <manujouve@gmail.com>
2018-05-01 13:18:48 +0200

    2nd

---
+++ db/migrate/20180501095016_creation_movies.rb
@@ -0,0 +1,9 @@
+class CreationMovies < ActiveRecord::Migration[5.2]
+  def change
+  	create_table :movies do |t|
+      t.string :title
+      t.integer :release_date
+      t.string :director
+  end
+end
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,9 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+Movie.destroy_all
+
+10.times do |index|
+
+  a = Movie.create(title: "Avengers", release_date: "2018", director: "M.connery")
+  a = Movie.create(title: "Moi moche mechant", release_date: "2013", director: "Mairje")
+  a = Movie.create(title: "Moi la france", release_date: "2011", director: "MMcallister")  
+
+end
